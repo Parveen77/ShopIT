0e139c8272a0ec7d5ed3cb44a12c0701287fe9f0
parveen77 <parveenbugalia77@gmail.com>
2024-09-22 07:56:35 +0530

    User Reviews

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,7 +4,10 @@ import {
         getProducts,
         newProduct,
         getProductDetails,
-        updateProduct,      
+        updateProduct,
+        createProductReview,
+        getProductReviews,
+        deleteReview,      
 } from '../controllers/productControllers.js';
 import { authorizeRoles, isAuthenticatedUser } from '../middlewares/auth.js'
 
@@ -17,5 +20,10 @@ router.route("/products/:id").get(getProductDetails);
 router.route("/products/:id").put(isAuthenticatedUser,authorizeRoles("admin"), updateProduct);
 router.route("/products/:id").delete(isAuthenticatedUser,authorizeRoles("admin"), deleteProduct);
 
+router.route("/reviews")
+.put(isAuthenticatedUser, createProductReview)
+.get(isAuthenticatedUser, getProductReviews);
+
+router.route("/admin/reviews").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
 
 export default router; 
